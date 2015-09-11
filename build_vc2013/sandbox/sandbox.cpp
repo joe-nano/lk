8febfac3c4e6ba0a048a932c2648fb1b79d7075d
Aron Dobos <aron.dobos@outlook.com>
2015-09-11 21:10:17 +0000

    rough structure for vm design and testing
    git-svn-id: https://efmsvn2.nrel.gov/lk/svn/branches/vm@150 beec71b5-fff0-e442-8ac2-4f84a11f714e

--- build_vc2013/sandbox/sandbox.cpp
+++ build_vc2013/sandbox/sandbox.cpp
@@ -1,3 +1,5 @@
+#define LK_USE_WXWIDGETS 1
+
 #include <wx/wx.h>
 #include <wx/frame.h>
 #include <wx/stc/stc.h>
@@ -14,100 +16,104 @@
 #include <lk_eval.h>
 #include <lk_lex.h>
 #include <lk_invoke.h>
-#include <lk_math.h>
 #include <lk_parse.h>
 
-#define LK_USE_WXWIDGETS 1
+enum { ID_CODE = wxID_HIGHEST+149, ID_PARSE, ID_ASM };
 
-
-
-#include <vector>
-#include <string>
-#include <algorithm>
-#include <allocators>
-
-typedef struct DIR DIR;
-struct dirent
+lk_string lk_code_gen( lk::node_t *node )
 {
-	char *d_name;
-};
-DIR           *opendir(const char *);
-int           closedir(DIR *);
-struct dirent *readdir(DIR *);
-void          rewinddir(DIR *);
+	return "";
+}
 
-class TestApp : public wxApp
+class VMTestFrame : public wxFrame
+{
+	wxTextCtrl *m_code, *m_parse, *m_asm;
+public:
+	VMTestFrame() : wxFrame( NULL, wxID_ANY, "LK-VM", wxDefaultPosition, wxSize(1200,900) )
+	{
+		wxFont font( 12, wxFONTFAMILY_MODERN, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_NORMAL, false, "Consolas" );
+
+		m_code = new wxTextCtrl( this, ID_CODE, wxEmptyString, wxDefaultPosition, wxDefaultSize, wxTE_MULTILINE );
+		m_code->SetFont( font );
+
+		m_parse = new wxTextCtrl( this, ID_PARSE, wxEmptyString, wxDefaultPosition, wxDefaultSize, wxTE_MULTILINE );
+		m_parse->SetFont( font );
+		m_parse->SetForegroundColour( *wxBLUE );
+
+		m_asm = new wxTextCtrl( this, ID_ASM, wxEmptyString, wxDefaultPosition, wxDefaultSize, wxTE_MULTILINE );
+		m_asm->SetFont( font );
+		m_asm->SetForegroundColour( "Dark green" );
+
+		wxBoxSizer *sizer = new wxBoxSizer( wxHORIZONTAL );
+		sizer->Add( m_code, 1, wxALL|wxEXPAND, 0 );
+		sizer->Add( m_parse, 1, wxALL|wxEXPAND, 0 );
+		sizer->Add( m_asm, 1, wxALL|wxEXPAND, 0 );
+		SetSizer(sizer);
+
+		m_code->LoadFile( wxGetHomeDir() + "/.lk-vm-code" );
+		ParseAndGenerateAssembly();
+	}
+
+	void ParseAndGenerateAssembly()
+	{
+		wxString output, assembly;
+		lk::input_string input( m_code->GetValue() );
+		lk::parser parse( input );
+		if ( lk::node_t *node = parse.script() )
+		{
+			if ( parse.error_count() == 0 )
+			{
+				lk::pretty_print( output, node, 0 );
+				assembly = lk_code_gen( node );
+			}
+			else
+			{
+				for( int i=0;i<parse.error_count();i++ )
+					output += parse.error(i) + "\n";
+			}
+
+			delete node;
+		}
+		else
+			output = "invalid parse: no nodes obtained";
+
+
+		m_parse->ChangeValue( output );
+		m_asm->ChangeValue( assembly );
+
+	}
+
+	void OnClose( wxCloseEvent &evt )
+	{
+		m_code->SaveFile( wxGetHomeDir() + "/.lk-vm-code" );
+		wxTheApp->ScheduleForDestruction( this );
+	}
+
+	void OnCommand( wxCommandEvent &evt )
+	{
+		if ( evt.GetId() == ID_CODE )
+			ParseAndGenerateAssembly();
+	}
+
+	DECLARE_EVENT_TABLE()
+};
+
+BEGIN_EVENT_TABLE( VMTestFrame, wxFrame )
+	EVT_CLOSE( VMTestFrame::OnClose )
+	EVT_TEXT( ID_CODE, VMTestFrame::OnCommand )
+END_EVENT_TABLE()
+
+
+class VMTestApp : public wxApp
 {
 public:
-	bool OnInit()
+	VMTestApp() { }
+
+	virtual bool OnInit()
 	{
-		wxFrame *frm = new wxFrame(NULL, wxID_ANY, "Test", wxDefaultPosition, wxSize(300, 200));
-		frm->SetBackgroundColour(*wxWHITE);
-
-		std::vector< lk_string  > x;
-		//lk::dir_exists("c:/test_csv");
-		lk_string path = "c:/test_csv";
-		lk_string extlist = "csv";
-
-		//x.push_back(d);
-		//x=lk::dir_list(d, e, false);
-		//bool b=lk::file_exists("c:\tmp");
-
-
-
-		// test dir_list
-		std::vector< lk_string > list;
-		std::vector< lk_string > extensions;// = split(lower_case(extlist), ",");
-		extensions.push_back("csv");
-		DIR *dir;
-		struct dirent *ent;
-
-		dir = ::opendir((const char*)path.c_str());
-//		dir = ::opendir((const wchar_t*)path.c_str());
-		 
-		while ((ent = readdir(dir)))
-		{
-			lk_string item(ent->d_name);
-			if (item == "." || item == "..")
-				continue;
-
-			//if (extlist.empty()
-			//	|| extlist == "*"
-			//	|| (ret_dirs && dir_exists((const char*)lk_string(path + "/" + item).c_str())))
-			//{
-			//	list.push_back(item);
-			//}
-			//else
-			//{
-				bool found = false;
-				lk_string ext = item;
-				for (size_t i = 0; !found && i<extensions.size(); i++)
-				{
-					if (ext == extensions[i])
-					{
-						list.push_back(item);
-						found = true;
-					}
-				}
-//			}
-		}
-
-		closedir(dir);
-
-
-		/* test opendir and readdir x64 and win32 - both working
-		struct DIR *dir;
-		struct dirent *ent;
-
-		dir = opendir((const char*)d.c_str());
-
-		ent = readdir(dir);
-		*/
-		
-		frm->Show();
+		(new VMTestFrame())->Show();
 		return true;
 	}
 };
 
-
-IMPLEMENT_APP(TestApp);
+IMPLEMENT_APP(VMTestApp);
