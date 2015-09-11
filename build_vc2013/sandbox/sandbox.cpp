8c62575bbd584295df4059bb585d0e10da4e9df5
Aron Dobos <aron.dobos@outlook.com>
2015-09-11 22:36:12 +0000

    some more updates
    git-svn-id: https://efmsvn2.nrel.gov/lk/svn/branches/vm@151 beec71b5-fff0-e442-8ac2-4f84a11f714e

--- build_vc2013/sandbox/sandbox.cpp
+++ build_vc2013/sandbox/sandbox.cpp
@@ -18,12 +18,213 @@
 #include <lk_invoke.h>
 #include <lk_parse.h>
 
-enum { ID_CODE = wxID_HIGHEST+149, ID_PARSE, ID_ASM };
 
-lk_string lk_code_gen( lk::node_t *node )
+namespace lk {
+
+class code_gen
 {
-	return "";
-}
+private:
+	int m_labelCounter;
+	lk_string m_output;
+	lk_string m_error;
+
+public:
+	code_gen() {
+		m_labelCounter = 0;
+	}
+
+	lk_string assembly() { return m_output; }
+	lk_string error() { return m_error; }
+
+	lk_string label()
+	{
+		return lk::format( "L%d", m_labelCounter++ );
+	}
+
+	void emit( const lk_string &s )
+	{
+		if ( s.IsEmpty() ){
+			m_output += "\n";
+			return;
+		}
+
+		if ( s[0] == 'L' )
+			m_output += s + "\n";
+		else
+			m_output += "   " + s + "\n";
+	}
+
+	enum Ctl { FAIL, OK, BREAK, CONTINUE, RETURN, EXIT };
+
+	Ctl eval( lk::node_t *root )
+	{
+		if ( !root ) return OK;
+
+		if ( list_t *n = dynamic_cast<list_t*>( root ) )
+		{
+			while( n )
+			{
+				if ( !eval( n->item ) )
+					return FAIL;
+
+				n=n->next;
+			}
+		}
+		else if ( iter_t *n = dynamic_cast<iter_t*>( root ) )
+		{
+			if ( n->init && !eval( n->init ) ) return FAIL;
+
+			// labels for beginning and end of loop
+			lk_string Lb = label();
+			lk_string Le = label();
+
+			emit( Lb ) ;
+
+			if ( !eval( n->test ) ) return FAIL;
+
+			emit( "jf " + Le );
+
+			Ctl c = eval( n->block );
+			switch( c )
+			{
+			case FAIL: return FAIL;
+			case BREAK: emit( "j " + Lb ); break;
+			case EXIT: emit( "end" ); break;
+			case RETURN: emit("ret"); break;
+			case CONTINUE:
+			case OK:
+				break;
+			}
+
+
+			if ( n->adv && !eval( n->adv ) ) return FAIL;
+
+			emit ("j " + Lb );
+			emit( Le );
+		}
+		else if ( cond_t *n = dynamic_cast<cond_t*>( root ) )
+		{	
+			lk_string L1 = label();
+			lk_string L2 = L1;
+
+			eval( n->test );
+			emit( "jf " + L1 );
+			eval( n->on_true );
+			if ( n->on_false )
+			{
+				L2 = label();
+				emit( "j " + L2 );
+				emit( L1 );
+				eval( n->on_false );
+			}
+			emit( L2 );
+		}
+		else if ( expr_t *n = dynamic_cast<expr_t*>( root ) )
+		{
+			switch( n->oper )
+			{
+			case expr_t::PLUS:
+				eval( n->left );
+				eval( n->right );
+				emit( "add" );
+				break;
+			case expr_t::MINUS:
+				eval( n->left );
+				eval( n->right );
+				emit( "sub" );
+				emit( "neg" );
+				break;
+			case expr_t::MULT:
+				eval( n->left );
+				eval( n->right );
+				emit( "mul" );
+				break;
+			case expr_t::DIV:
+				eval( n->left );
+				eval( n->right );
+				emit( "div" );
+				break;
+			case expr_t::LT:
+				eval( n->left );
+				eval( n->right );
+				emit( "lt" );
+				break;
+			case expr_t::GT:
+				eval( n->left );
+				eval( n->right );
+				emit( "gt" );
+				break;
+			case expr_t::LE:
+				eval( n->left );
+				eval( n->right );
+				emit( "le" );
+				break;
+			case expr_t::GE:
+				eval( n->left );
+				eval( n->right );
+				emit( "ge" );
+				break;
+			case expr_t::NE:
+				eval( n->left );
+				eval( n->right );
+				emit( "ne" );
+				break;
+			case expr_t::EQ:
+				eval( n->left );
+				eval( n->right );
+				emit( "eq" );
+				break;
+			case expr_t::ASSIGN:
+				{
+					if ( !eval( n->right ) ) return FAIL;
+
+					bool sset = false;
+					// if on the LHS of the assignment we have a special variable i.e. ${xy}, use a 
+					// hack to assign the value to the storage location
+					if ( lk::iden_t *iden = dynamic_cast<lk::iden_t*>(n->left) )
+					{
+						if ( iden->special )
+						{
+							emit( "store! " + iden->name );
+							return OK;
+						}
+					}
+
+					if ( !eval( n->left ) ) return FAIL;
+					emit( "store" );
+				}
+				break;
+			default:
+				m_error = lk_string("unsupported expression: ") + n->operstr();
+				return FAIL;
+			}
+		}
+		else if ( iden_t *n = dynamic_cast<iden_t*>( root ) )
+		{			
+			if ( n->special )
+			{
+				emit( "load! " + n->name );
+				return OK;;
+			}
+			else
+				emit( "load " + n->name );
+		}
+		else if ( constant_t *n = dynamic_cast<constant_t*>(root ) )
+		{
+			emit( "push " + lk::format("%lg",n->value) );
+		}
+		else if ( literal_t *n = dynamic_cast<literal_t*>(root ) )
+		{
+			emit( "push '" + n->value + "'" );
+		}
+
+		return OK;
+	}
+};
+
+}; // namespace lk
+
+enum { ID_CODE = wxID_HIGHEST+149, ID_PARSE, ID_ASM };
 
 class VMTestFrame : public wxFrame
 {
@@ -64,7 +265,9 @@ public:
 			if ( parse.error_count() == 0 )
 			{
 				lk::pretty_print( output, node, 0 );
-				assembly = lk_code_gen( node );
+				lk::code_gen cg;
+				if ( cg.eval( node ) ) assembly = cg.assembly();
+				else assembly = cg.error();
 			}
 			else
 			{
